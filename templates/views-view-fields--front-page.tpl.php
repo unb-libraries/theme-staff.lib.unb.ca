9f11c46915a50bd6cdb2368dd4085642a1f4509a
JeremyMcDermott <jeremy.mcdermott@gmail.com>
2015-05-08 15:49:40 -0300

    Comment Front page news theme template as per Drupal commenting standards

--- templates/views-view-fields--front-page.tpl.php
+++ templates/views-view-fields--front-page.tpl.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * @file
+ * Handle stickied front page news content.
+ *
+ * Check "Front page news" content type nodes for a "Sticky at top of lists"
+ * publishing option. Wrap any "Front page news" Views rows containing stickied
+ * content in a container with a "sticky" class to enable CSS styling.
+ */
+?>
 <?php print ($row->_field_data['nid']['entity']->sticky) ? '<div class="sticky">' : ''; ?>
 
 <?php foreach ($fields as $id => $field): ?>
