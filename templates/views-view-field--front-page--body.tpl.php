82e09fa783619aec91afca1d3954a2b3ed2314f5
JeremyMcDermott <jeremy.mcdermott@gmail.com>
2015-05-11 13:47:10 -0300

    changed field value access from Drupal 6 style to proper Drupal 7 using 'get_field_items' function; added comments

--- templates/views-view-field--front-page--body.tpl.php
+++ templates/views-view-field--front-page--body.tpl.php
@@ -1,6 +1,29 @@
-<?php $myRawOutput = $row->field_body[0]['raw']['safe_value']; ?>
+<?php
+/**
+ * @file
+ * Handle "Front page news" Views row body text.
+ *
+ * Compares "Front page news" content type body field's "raw safe value" &
+ * "rendered output" values: equal values imply that no characters were trimmed,
+ * making a "More" link redundant. Untrimmed teaser body fields are wrapped in a
+ * container with "untrimmed" class allowing styling (i.e. hiding) "More" links.
+ */
+?>
+<?php
+  // The node id is contained in the Views $row variable.
+  $body_items = field_get_items('node', node_load($row->nid), 'body');
+?>
 
-<?php if ($myRawOutput == $output): ?>
+<?php if ($body_items): ?>
+  <?php
+    // Get [0] element of $body_items array.
+    $first_body_item = reset($body_items);
+    // Get raw, body field 'safe value': includes beginning/ending whitespace.
+    $raw_body_value = $first_body_item['safe_value'];
+  ?>
+<?php endif; ?>
+
+<?php if ($raw_body_value == $output): ?>
   <div class="untrimmed">
     <?php print $output; ?>
   </div>
